e3b6e1b8c137aa453ed01e37c7da6bb3dd9c6925
Tazeenamir <tazeen75@icloud.com>
2025-01-03 01:08:51 +0500

    h

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,7 +6,7 @@ const connection=mysql.createPool({
     host: "127.0.0.1",
     user: "root",
     password: "root",
-    database: "learn",
+    database: "newschema",
     port: 3306,
     ssl: false
     // ssl: false
