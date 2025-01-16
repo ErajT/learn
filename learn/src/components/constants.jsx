85080a8f18f90c22036b4c53d0e25171efa83641
Eraj Tanweer <erajtanweer2@gmail.com>
2025-01-16 13:03:36 +0500

    abc

--- learn/src/components/constants.jsx
+++ learn/src/components/constants.jsx
@@ -1,1 +1,2 @@
-export const backendUrl = "https://backend-5862xf61k-erajts-projects.vercel.app";
+// export const backendUrl = "https://backend-5862xf61k-erajts-projects.vercel.app";
+export const backendUrl = "http://localhost:2000";
