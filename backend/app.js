119e3a2f902be2c738d8c3f905fa3c14983f91f8
Eraj Tanweer <erajtanweer2@gmail.com>
2025-01-16 12:35:29 +0500

    abc

--- backend/app.js
+++ backend/app.js
@@ -11,16 +11,16 @@ let app = express();
 
 // Middleware to enable CORS
 app.use((req, res, next) => {
-  // res.setHeader('Access-Control-Allow-Origin', 'https://learn-lime-three.vercel.app'); // Adjust the origin as needed
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); 
+  res.setHeader('Access-Control-Allow-Origin', 'https://learn-nmuga4b6l-erajts-projects.vercel.app/'); // Adjust the origin as needed
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
 app.use(cors({
-  // origin: 'https://learn-lime-three.vercel.app', // Allow requests from frontend application
-  origin: 'http://localhost:5173',
+  origin: 'https://learn-nmuga4b6l-erajts-projects.vercel.app/', // Allow requests from frontend application
+  // origin: 'http://localhost:5173',
   credentials: true // Allow credentials (cookies) to be included with requests
 }));
 
--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,22 @@
-const mysql = require('mysql2');
+const mysql = require('mysql');
 //Database Connection
 
 const connection=mysql.createPool({
+    // connectionLimit: 6,
+    // host: "127.0.0.1",
+    // user: "root",
+    // password: "root",
+    // database: "learn",
+    // port: 3306,
+    // ssl: false
+    // // ssl: false
     connectionLimit: 6,
-    host: "127.0.0.1",
-    user: "root",
-    password: "root",
+    host: "qec-db-3.mysql.database.azure.com",
+    user: "qecproject",
+    password: "software22NED",
     database: "learn",
     port: 3306,
-    ssl: false
-    // ssl: false
+    ssl: true
 })
 connection.getConnection((err,connection)=>{
     if (err){
