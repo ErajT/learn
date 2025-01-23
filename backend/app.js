14d4c9a935d7c098a356681c8df759edf51df873
Eraj Tanweer <erajtanweer2@gmail.com>
2025-01-23 20:23:15 +0500

    abcd

--- backend/app.js
+++ backend/app.js
@@ -11,19 +11,20 @@ const ScheduleRouter = require('./Routes/ScheduledRoutes');
 let app = express();
 app.options('*', cors()); // Allow preflight requests
 
-// Middleware to enable CORS
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://learn-lime-three.vercel.app'); // Adjust the origin as needed
-  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); 
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true'); 
-  next();
-});
+// // Middleware to enable CORS
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'https://learn-lime-three.vercel.app'); // Adjust the origin as needed
+//   // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); 
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true'); 
+//   next();
+// });
 
 app.use(cors({
-  origin: 'https://learn-lime-three.vercel.app', // Allow requests from frontend application
+  origin: ['http://localhost:5173', 'https://learn-lime-three.vercel.app'], // Allow requests from frontend application
   // origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true // Allow credentials (cookies) to be included with requests
 }));
 
