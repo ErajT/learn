aaf45a50309732f61ba2971f71d5f9ba297aaeba
Eraj Tanweer <erajtanweer2@gmail.com>
2025-01-23 18:21:13 +0500

    abcd

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.options('*', cors()); // Allow preflight requests
 
 // Middleware to enable CORS
 app.use((req, res, next) => {
-  // res.setHeader('Access-Control-Allow-Origin', 'https://learn-nmuga4b6l-erajts-projects.vercel.app'); // Adjust the origin as needed
+  res.setHeader('Access-Control-Allow-Origin', 'https://learn-lime-three.vercel.app'); // Adjust the origin as needed
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  // origin: 'https://learn-nmuga4b6l-erajts-projects.vercel.app', // Allow requests from frontend application
+  origin: 'https://learn-lime-three.vercel.app', // Allow requests from frontend application
   origin: 'http://localhost:5173',
   credentials: true // Allow credentials (cookies) to be included with requests
 }));
