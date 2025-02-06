e1cde9cbbe9b27661a69cc88e2f9d4e4a83063df
Eraj Tanweer <erajtanweer2@gmail.com>
2025-02-06 20:59:32 +0500

    abcd

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.options('*', cors()); // Allow preflight requests
 
 // Middleware to enable CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://deliberatives.com/'); // Adjust the origin as needed
+  res.setHeader('Access-Control-Allow-Origin', 'https://deliberatives.com'); // Adjust the origin as needed
   // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: ['https://deliberatives.com/'], // Allow requests from frontend application
+  origin: ['https://deliberatives.com'], // Allow requests from frontend application
   // origin: ['https://learn-git-main-erajts-projects.vercel.app'],
   // origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
@@ -34,7 +34,7 @@ app.use(bodyParser.urlencoded({ limit: '200mb', extended: true }));
 
 
 const corsOptions = {
-  origin: ['https://deliberatives.com/'], // Allowed origins
+  origin: ['https://deliberatives.com'], // Allowed origins
   // origin: ['https://learn-git-main-erajts-projects.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Allowed methods
   credentials: true // Allow credentials
