0509dcc53afe12dc538350b43f9fc13ad3c46fcd
Eraj Tanweer <erajtanweer2@gmail.com>
2025-03-01 14:44:22 +0500

    finally

--- learn/src/components/constants.jsx
+++ learn/src/components/constants.jsx
@@ -1,5 +1,5 @@
 // export const backendUrl = "https://backend-snowy-delta.vercel.app";
 // require('dotenv').config();
 // export const backendUrl = process.env.BACKEND_URL;
-// export const backendUrl = "https://deliberatives.com/api";
-export const backendUrl = "http://localhost:2000"
+export const backendUrl = "https://deliberatives.com/api";
+// export const backendUrl = "http://localhost:2000"
